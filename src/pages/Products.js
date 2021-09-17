50cca6baf5561f3f1346a8846f20c38a7dfb5dc3
Mitchell <mwhyuen@gmail.com>
2021-09-17 10:45:00 +0200

    linking products to product details

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,15 +5,15 @@ const Products = () => {
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <Link>
-          <li>A Book</li>
-        </Link>
-        <Link>
-          <li>A Carpet</li>
-        </Link>
-        <Link>
-          <li>An Online Course</li>
-        </Link>
+        <li>
+          <Link to="product/1">A Book</Link>
+        </li>
+        <li>
+          <Link to="product/2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="product/3">An Online Course</Link>
+        </li>
       </ul>
     </section>
   );
