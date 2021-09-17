4d999e048195f17bc9e00c6ba6f9c56f3de44e64
Mitchell <mwhyuen@gmail.com>
2021-09-17 10:41:48 +0200

    feat: add ProductDetails

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,12 @@
+import { useParams } from 'react-router-dom'
+
 const ProductDetail = () => {
-  return (
+    const params = useParams();
+    
+    return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
