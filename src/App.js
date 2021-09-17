7045ee560416cec304a58868486b5f9aaa1c8e08
Mitchell <mwhyuen@gmail.com>
2021-09-17 10:21:02 +0200

    setting up dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
-import MainHeader from './components/MainHeader'
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product/:productId">
+          <ProductDetail />
+        </Route>
       </main>
     </div>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-    return <h1>The Products Page</h1>
-}
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <Link>
+          <li>A Book</li>
+        </Link>
+        <Link>
+          <li>A Carpet</li>
+        </Link>
+        <Link>
+          <li>An Online Course</li>
+        </Link>
+      </ul>
+    </section>
+  );
+};
 
 export default Products;
